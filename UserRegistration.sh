6223a8e5d0b8a962f1178b2e19eb44a2b364c628
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-07-15 12:54:55 +0530

    valid first name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "Welcome to Patterns"
-
+read -p "Enter  User First Name " name
+pat="^[A-Z]{1}[a-z]{3,}$"
+if [[ $name =~ $pat ]]
+then
+	echo "FirstName is valid "
+else
+	echo "FirstName is invalid"
+fi
