f403a28e5dbf5faa1bf993c35b7def925be0e805
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-07-15 15:30:55 +0530

    added Password rule4

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -39,15 +39,26 @@ fi
 read -p "Enter Password:" password
 password_pat1="^.{8,}$"
 password_pat2="[[:upper:]]{1,}"
+password_pat3="[0-9]{1,}"
+password_pat4="([a-zA-Z0-9]*)[@!#%<>()/?&*]{1}([a-zA-Z0-9]*)$"
 if [[ $password =~ $password_pat1 ]]
 then
 	if [[ $password =~ $password_pat2 ]]
 	then
-        	echo "Password is valid"
+		if [[ $password =~ $password_pat3 ]]
+		then
+			if [[ $password =~ $password_pat4 ]]
+        		then
+				echo "Password is valid"
+			else
+				echo "Password must have atleast 1 special character"
+			fi
+		else
+			echo "Password should  have atleast 1 numeric number"
+		fi
 	else
 		echo "Password should  have atleast 1 UpperCase"
 	fi
 else
         echo "Password should have minimum 8 Characters"
 fi
-
